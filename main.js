da82326639449158ea1b77a0e82a13c1564b99e3
Rohan Prinja <rohan.prinja@gmail.com>
2014-12-21 02:37:07 +0530

    idk

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600, 400, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameDiv');
 
 game.global = {
   score: 0
--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 var player;
 
 var playerConsts = {
-    MAX_SPEED: 500,
+    MAX_SPEED: 800,
     GRAVITY: 2600,
     JUMP_SPEED: -1000
 };
@@ -11,52 +11,55 @@ var playState = {
 
   create: function () {
     console.log("play create");
-    
+
     player = game.add.sprite(game.width/2 - 50, game.height/2 - 50, 'player');
-    player.scale.setTo(0.2, 0.2);
+    player.scale.setTo(0.1, 0.1);
     player.anchor.setTo(0.5, 0.5);
-      
+
     game.physics.enable(player, Phaser.Physics.ARCADE);
     player.body.collideWorldBounds = true;
-      
+
     player.body.maxVelocity.setTo(playerConsts.MAX_SPEED, playerConsts.MAX_SPEED*10);
     game.physics.arcade.gravity.y = playerConsts.GRAVITY;
-      
+
     this.canDoubleJump = true;
-    
+
     game.time.advancedTiming = true;
     this.fpsText = game.add.text(20, 20, '', { font: '16px Arial', fill: '#000000' });
-    
+
     game.input.keyboard.addKeyCapture([
       Phaser.Keyboard.LEFT,
       Phaser.Keyboard.RIGHT,
       Phaser.Keyboard.UP,
       Phaser.Keyboard.DOWN
     ]);
-    
+    // this.minHeight = 1000;
   },
-  
+
+  // http://gamemechanicexplorer.com/#platformer-5
   update: function () {
     if (game.time.fps != 0) {
+      // if (player.body.position.y < this.minHeight) this.minHeight = player.body.position.y;
+      // this.fpsText.setText('height ' + Math.floor(this.minHeight));
       this.fpsText.setText(game.time.fps + ' FPS');
     }
-      
+
     if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        player.body.velocity.x = -playerConsts.MAX_SPEED;
+      player.body.velocity.x = -playerConsts.MAX_SPEED;
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        player.body.velocity.x = playerConsts.MAX_SPEED;
+      player.body.velocity.x = playerConsts.MAX_SPEED;
     } else {
-        player.body.velocity.x = 0;
+      player.body.velocity.x = 0;
     }
-      
+
     var onTheGround = player.body.touching.down || player.body.blocked.down;
     if (onTheGround) this.canDoubleJump = true;
-      
+
     if (game.input.keyboard.downDuration(Phaser.Keyboard.UP, 5)) {
-        if (this.canDoubleJump || onTheGround) {
-            player.body.velocity.y = playerConsts.JUMP_SPEED;
-            if (!onTheGround) this.canDoubleJump = false;
-        }
+      if (this.canDoubleJump || onTheGround) {
+        player.body.velocity.y = playerConsts.JUMP_SPEED;
+        if (!onTheGround) this.canDoubleJump = false;
+      }
     }
   }
 };
