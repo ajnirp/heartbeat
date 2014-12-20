1b90d611d46b3ada2e173db2599f53c6a12ea8e1
Rohan Prinja <rohan.prinja@gmail.com>
2014-12-21 00:01:08 +0530

    phaser version upgrade

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ var menuState = {
     // var titleText = game.add.bitmapText(0, 0, '04b19', 'Space Shooter', 64);
     // titleText.x = game.width/2 - titleText.textWidth/2;
     // titleText.y = game.height/2 - titleText.textHeight/2 - 50;
-    
+
     // var pressStartText = game.add.bitmapText(0, 0, '04b19', 'press space to start', 24);
     // pressStartText.x = game.width/2 - pressStartText.textWidth/2;
     // pressStartText.y = game.height/2 - pressStartText.textHeight/2 + 40;
