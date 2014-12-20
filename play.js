752320125a514156cde830c71cdaeda06c832aa7
Rohan Prinja <rohan.prinja@gmail.com>
2014-12-20 23:55:08 +0530

    cleanup

--- play.js
+++ play.js
@@ -20,27 +20,27 @@ var jumpButton;
 
 var playState = {
   fpsText: '',
-  
+
   create: function () {
     console.log("play create");
     game.physics.arcade.gravity.y = 250;
-    
+
     player = game.add.sprite(game.width/2 - 50, game.height/2 - 50, 'player');
-    
+
     // scale down the player sprite
     player.scale.setTo(0.5, 0.5);
-    
+
     // the player anchor should be at (0.5, 0.5)
     // otherwise animations look jerky because the default anchor is at (0, 0)
     player.anchor.setTo(0.5, 0.5);
-    
+
     // add animations to the player
     // the second last argument determines whether looping should be done or not
     player.animations.add('idleleft', [0], 60, false, true);
     player.animations.add('idleright', [1], 60, false, true);
     player.animations.add('runningleft', [2,3,4,5,6,7,8,9,10], 20, true, true);
     player.animations.add('runningright', [11,12,13,14,15,16,17,18,19], 20, true, true);
-  
+
     // enable physics on the player
     game.physics.enable(player, Phaser.Physics.ARCADE);
     // prevent player from leaving the world
@@ -50,11 +50,11 @@ var playState = {
     // add drag and gravity
     player.body.drag.setTo(playerConstants.DRAG, 0);
     game.physics.arcade.gravity.y = worldConstants.GRAVITY;
-    
+
     // show FPS
     game.time.advancedTiming = true;
     this.fpsText = game.add.text(20, 20, '', { font: '16px Arial', fill: '#000000' });
-                                    
+
     // capture keyboard input
     game.input.keyboard.addKeyCapture([
       Phaser.Keyboard.LEFT,
@@ -62,20 +62,20 @@ var playState = {
       Phaser.Keyboard.UP,
       Phaser.Keyboard.DOWN
     ]);
-    
+
 //    cursors = game.input.keyboard.createCursorKeys();
 //    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   },
-  
+
 //  loadUpdate: function () {
 //  },
-  
+
   update: function () {
     // update FPS
     if (game.time.fps != 0) {
       this.fpsText.setText(game.time.fps + ' FPS');
     }
-    
+
     // take keyboard input
     // if the player is facing in the same direction as the keyboard input, accelerate him
     // otherwise, turn him around
@@ -99,7 +99,7 @@ var playState = {
     else {
       player.body.acceleration.x = 0;
     }
-    
+
     // choose the right animation for the player
     var playerMovement = (player.body.velocity.x == 0 ? 'idle' : 'running');
     player.animations.play(playerMovement + playerData.facing);
